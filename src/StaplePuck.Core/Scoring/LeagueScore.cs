e4d3832d04338f77a535a4d9e65527485ce47ef2
nickhetland <nick.hetland@infiniteleap.net>
2018-12-04 19:16:34 -0600

    Add scoring models

---
+++ src/StaplePuck.Core/Scoring/LeagueScore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StaplePuck.Core.Fantasy;
+
+namespace StaplePuck.Core.Scoring
+{
+    public class LeagueScore
+    {
+        public League League { get; set; }
+        public IEnumerable<TeamScore> Teams { get; set; }
+        public DateTime Date { get; set; }
+        public Dictionary<int, PlayerScore> Players { get; private set; }
+    }
+}
---
+++ src/StaplePuck.Core/Scoring/PlayerScore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StaplePuck.Core.Stats;
+
+namespace StaplePuck.Core.Scoring
+{
+    public class PlayerScore
+    {
+        public Player Player { get; set; }
+        public Team Team { get; set; }
+        public List<ScoreItem> Scoring { get; set; }
+        public int Count { get; set; }
+        public GameState GameState { get; set; }
+
+        public int Score
+        {
+            get { return Scoring.Sum(x => x.Score); }
+        }
+        public int TodaysScore
+        {
+            get { return Scoring.Sum(x => x.TodaysScore); }
+        }
+    }
+}
