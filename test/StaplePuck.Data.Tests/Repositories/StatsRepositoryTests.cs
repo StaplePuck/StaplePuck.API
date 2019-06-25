24dd11241fe36ec17ed245954c1bab3a45ee48f9
nickhetland <nick.hetland@infiniteleap.net>
2019-06-24 19:22:27 -0500

    Add unit test project

---
+++ test/StaplePuck.Data.Tests/Repositories/StatsRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using StaplePuck.Data;
+using StaplePuck.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StaplePuck.Data.Tests.Repositories
+{
+    public class StatsRepositoryTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var options = new DbContextOptionsBuilder<StaplePuckContext>()
+                .UseInMemoryDatabase(databaseName: "DBCreation")
+                .Options;
+
+            using (var context = new StaplePuckContext(options))
+            {
+                var repo = new StatsRepository(context);
+                //repo.Add();
+            }
+            //var repo = new StatsRepository();
+        }
+    }
+}
